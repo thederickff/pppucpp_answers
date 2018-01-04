74a64596a136882e1f221a22fd5f882bea3a6820
derickfelix <derickfelix4321@gmail.com>
2018-01-03 21:21:39 -0500

    Add * and / signs

--- Part01/06.Writing-a-Program/calculator00.cpp
+++ Part01/06.Writing-a-Program/calculator00.cpp
@@ -1,21 +1,47 @@
 #include "../std_lib_facilities.h"
 
+class Token { // a very simple user-defined type
+public:
+    char kind;
+    double value;
+};
+
+Token get_token(); // function to read a token from cin
+vector<Token> tok; // we'll put the tokens here
+
 int main()
 {
-    cout << "Please enter expression (we can handler + and -): ";
+    cout << "Please enter expression (we can handler + and -, *, and /):\n";
+    cout << "add an x to end expression (e.g., 1+2*3x): ";
 
     int lval = 0;
     int rval;
-    char op;
-    int res;
-    cin>>lval>>op>>rval; // read something like 1 + 3
+    cin>>lval; // read leftmost operand
+    if (!cin) error("no first operand");
+    
+    for (char op; cin >> op; ) // read operator and right-hand operand repeatedly
+    {
+        if (op!='x') cin>>rval;
+        if (!cin) error("no second operand");
+        switch(op) {
+            case '+':
+                lval += rval;
+                break;
+            case '-':
+                lval -= rval;
+                break;
+            case '*':
+                lval *= rval;
+                break;
+            case '/':
+                lval /= rval;
+                break;
+            default:
+                cout << "Result: " << lval << '\n';
+                keep_window_open();
+                return 0;
+        }
+    }
 
-    if (op=='+')
-        res = lval + rval;
-    else if (op=='-')
-        res = lval - rval;
-
-    cout << "Result: " << res << '\n';
-
-    return 0;
+    error("bad expression");
 }
