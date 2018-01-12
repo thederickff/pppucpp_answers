c5bf15b8ac864507d47961ce03186c09565da725
derickfelix <derickfelix4321@gmail.com>
2018-01-11 22:33:53 -0500

    Create drill.cpp

---
+++ Part01/02.Hello-World/drill.cpp
@@ -0,0 +1,8 @@
+#include "../std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, World\n"; // output "Hello, World!"
+    keep_window_open();
+    return 0;
+}
