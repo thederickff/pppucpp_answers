c1b6ee9dc46d1c4b989abfa00548e22a2b5e2a76
derickfelix <derickfelix4321@gmail.com>
2018-01-22 23:00:40 -0500

    Chapter 6: create ex04.cpp
    Define a class Name_value that holds a string and a value. Rework exercise
    19 in Chapter 4 to use a vector<Name_value> instead of two vectors.

--- Part01/06.Writing-a-Program/ex04.cpp
+++ Part01/06.Writing-a-Program/ex04.cpp
@@ -1,9 +1,20 @@
+/*
+    Define a class Name_value that holds a string and a value. Rework exercise
+    19 in Chapter 4 to use a vector<Name_value> instead of two vectors.
+*/
 #include "../std_lib_facilities.h"
+
+class Name_value {
+public:
+    string name;
+    int value;
+    Name_value(string n, int val) 
+    : name(n), value(val) { }
+};
 
 int main()
 {
-    vector<string> names;
-    vector<int> scores;
+    vector<Name_value> nvs;
 
     cout << "Enter a sequence of name-and-value pairs, type 'NoName 0' to stop.\n";
     while (true)
@@ -14,19 +25,19 @@ int main()
         if (name == "NoName" && score == 0)
             break;
 
-        for (int i = 0; i < names.size(); ++i) {
-            if (name == names[i]) {
+        for (int i = 0; i < nvs.size(); ++i) {
+            if (name == nvs[i].name) {
                 cerr << "Error, you tried to enter a name twice." << endl;
                 return 0;
             }
         }
-        names.push_back(name);
-        scores.push_back(score);
+        Name_value nv(name, score);
+        nvs.push_back(nv);
     }
 
     cout << "-----Showing list-----" << endl;
-    for (int i=0; i < names.size(); ++i) {
-        cout << names[i] << " " << scores[i] << endl;
+    for (int i=0; i < nvs.size(); ++i) {
+        cout << nvs[i].name << " " << nvs[i].value << endl;
     }
     return 0;
 }
