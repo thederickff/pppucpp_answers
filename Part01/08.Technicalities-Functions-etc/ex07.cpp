70b7e3c92c9eeea66ad1c0ccf0a1f5390924a657
derickfelix <derickfelix4321@gmail.com>
2018-02-25 07:33:48 -0500

    Chapter 08: create ex07.cpp

---
+++ Part01/08.Technicalities-Functions-etc/ex07.cpp
@@ -0,0 +1,18 @@
+/*
+  Read five strings into a vector<string> name, then prompt the user for
+  the ages of the people named and store the ages into a vector<double> age.
+  Then print out the (name[i], age[i]) pairs. The tricky part here is to get
+  the age vector in the correct order to match the sorted name vector. Hint:
+  Before sorting name, take a copy and use that to make a copy of age in the
+  right order after sorting name. Then, do that exercise again but allowing
+  an arbitrary number of names.
+*/
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
