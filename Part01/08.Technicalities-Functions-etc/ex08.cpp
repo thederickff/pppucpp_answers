eb78a7dc0a6bf684f44831040bb1ca669dba9e61
derickfelix <derickfelix4321@gmail.com>
2018-02-28 18:43:11 -0500

    Chapter 08: create ex08.cpp and ex09.cpp

---
+++ Part01/08.Technicalities-Functions-etc/ex08.cpp
@@ -0,0 +1,13 @@
+/*
+  Write a function that given two vector<double>s price and weight computes a
+  value (an "index") that is the sum of all price[i]*weight[i]. Make sure to
+  have weight.size() == price.size().
+*/
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+
+  return 0;
+}
---
+++ Part01/08.Technicalities-Functions-etc/ex09.cpp
@@ -0,0 +1,15 @@
+/*
+  Write a function maxv() that returns the largest element of a vector argument
+  and also computes the mean and the median. Do not use global variables. Either
+  return a struct containing the results or pass them back through reference
+  arguments. Which of the two ways of returning several result values do you
+  prefer and why?
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+
+
+  return 0;
+}
