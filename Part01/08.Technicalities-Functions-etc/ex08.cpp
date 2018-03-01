b7fbd3f6cf054764764af02648038dd9d5b69663
derickfelix <derickfelix4321@gmail.com>
2018-03-01 17:41:35 -0500

    Chapter 08: solve ex08.cpp
    Write a function that given two vector<double>s price and weight computes a
    value (an "index") that is the sum of all price[i]*weight[i]. Make sure to
    have weight.size() == price.size().

--- Part01/08.Technicalities-Functions-etc/ex08.cpp
+++ Part01/08.Technicalities-Functions-etc/ex08.cpp
@@ -6,8 +6,28 @@
 
 #include "../std_lib_facilities.h"
 
+void compute(const vector<double>& prices, const vector<double>& weights)
+{
+  if (prices.size() != weights.size()) {
+    cout << "Couldn't compute value, make sure the vectors have the same size.\n";
+    return;
+  }
+
+  double n = 0;
+
+  for (int i = 0; i < prices.size(); ++i) {
+    n += prices[i] * weights[i];
+  }
+
+  cout << "Index is " << n << '\n';
+}
+
 int main()
 {
+  vector<double> prices = {2.34, 0.92, 0.45, 1.59, 4.29};
+  vector<double> weights = {0.8, 0.5, 1.5, 2.5, 5.0};
+
+  compute(prices, weights);
 
   return 0;
 }
