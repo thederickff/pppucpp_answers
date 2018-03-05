f2c3347840d157faaa42be72f8e1cd85c6a9140e
derickfelix <derickfelix4321@gmail.com>
2018-03-04 21:11:38 -0500

    Chapter 08: solve ex10.cpp
    Write a function that finds the smallest and the largest element of a vector
    argument and also computes the mean and the median. Do not use global
    variables. Either return a struct containing the results or pass them back
    through reference arguments. Which of the two ways of returning several result
    values do you prefer and why?

--- Part01/08.Technicalities-Functions-etc/ex10.cpp
+++ Part01/08.Technicalities-Functions-etc/ex10.cpp
@@ -1,15 +1,17 @@
 /*
-  Write a function maxv() that returns the largest element of a vector argument
-  and also computes the mean and the median. Do not use global variables. Either
-  return a struct containing the results or pass them back through reference
-  arguments. Which of the two ways of returning several result values do you
-  prefer and why?
+  Write a function that finds the smallest and the largest element of a vector
+  argument and also computes the mean and the median. Do not use global
+  variables. Either return a struct containing the results or pass them back
+  through reference arguments. Which of the two ways of returning several result
+  values do you prefer and why?
 */
 #include "../std_lib_facilities.h"
 
-double maxv(vector<double>& array, double& mean, double& median)
+double compute(vector<double>& array, double& mean, double& median, double& smallest)
 {
   sort(array);
+  mean = median = smallest = 0;
+
   int mostRepeated = 0;
   for (int i = 0; i < array.size(); ++i) {
       int repeats = 0;
@@ -19,7 +21,6 @@ double maxv(vector<double>& array, double& mean, double& median)
           }
       }
 
-
       if (mostRepeated < repeats) {
           mostRepeated = repeats;
           mean = array[i];
@@ -27,16 +28,16 @@ double maxv(vector<double>& array, double& mean, double& median)
   }
 
   median = array[array.size() / 2];
-
+  smallest = array[0];
   return array[array.size() - 1];
 }
 
 int main()
 {
-  vector<double> array = {2.3, 4.5, 7.34, 2.23, 2.3, 7.1, 1.2, 0.8, 0.938, 4.5, 4.5};
-  double mean, median, largest;
+  vector<double> array = {2.3, 4.5, 7.34, 2.23, 12.3, 7.1, 1.2, 0.8, 0.938, 7.5, 1.5, 7.1};
+  double mean, median, largest, smallest;
 
-  largest = maxv(array, mean, median);
+  largest = compute(array, mean, median, smallest);
 
   for (double a : array) {
     cout << a << "\t";
@@ -46,6 +47,7 @@ int main()
   cout << "Mean: " << mean << '\n';
   cout << "Median: " << median << '\n';
   cout << "Largest: " << largest << '\n';
+  cout << "Smallest: " << smallest << '\n';
 
   return 0;
 }
