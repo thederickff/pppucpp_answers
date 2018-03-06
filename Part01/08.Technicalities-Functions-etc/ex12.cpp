df197a3809ac905524e70c96b27db459c66849de
derickfelix <derickfelix4321@gmail.com>
2018-03-06 19:46:42 -0300

    Chapter 08: create ex12.cpp

---
+++ Part01/08.Technicalities-Functions-etc/ex12.cpp
@@ -0,0 +1,14 @@
+/*
+  Write a function that takes an vector<string> argument and returns a
+  vector<int> containing the number of characters in each string. Also find the
+  longest and the shortest string and the lexicographically first and last
+  string. How many separate functions would you use for these tasks? Why?
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
