26551763055e77a817b1f81a31f1009accd45fb4
derickfelix <derickfelix4321@gmail.com>
2018-03-07 22:09:03 -0300

    Chapter 08: create ex13.cpp

---
+++ Part01/08.Technicalities-Functions-etc/ex13.cpp
@@ -0,0 +1,14 @@
+/*
+  Can we declare a non-reference function argument const
+  (e.g. void f(const int);)? What might it mean? Why might we want to do that?
+  Why don't people do that often? Try it; write a couplem of small programs to
+  see what works.
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+
+
+  return 0;
+}
