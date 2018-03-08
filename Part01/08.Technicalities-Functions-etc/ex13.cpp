1a8be0eebd41543a3a73f13016cbd4846ac2ec0c
derickfelix <derickfelix4321@gmail.com>
2018-03-07 22:29:25 -0300

    Chapter 08: solve ex13.cpp
    Can we declare a non-reference function argument const
    (e.g. void f(const int);)? What might it mean? Why might we want to do that?
    why don't people do that often?/ Try it: write a couple of small programs to
    see what works.

--- Part01/08.Technicalities-Functions-etc/ex13.cpp
+++ Part01/08.Technicalities-Functions-etc/ex13.cpp
@@ -1,14 +1,37 @@
 /*
   Can we declare a non-reference function argument const
   (e.g. void f(const int);)? What might it mean? Why might we want to do that?
-  Why don't people do that often? Try it; write a couplem of small programs to
+  Why don't people do that often? Try it; write a couple of small programs to
   see what works.
+
+  Yes, it is pretty much like creating a local constant, but with undefined
+  initialization. Because in some cases it may be unnecessary, or some people
+  don't know it could be possible.
 */
 #include "../std_lib_facilities.h"
 
+void f(const int a)
+{
+  if (a > 10) return;
+  f(a+1);
+  cout << "A: " << a << '\n';
+}
+
+void aProgression(int n1, const int r)
+{
+  if (n1 > r*100) return;
+  aProgression(n1+r, r);
+  cout << "n1: " << n1 << ", r: " << r << '\n';
+}
+
+
 int main()
 {
+  int i = 0;
 
+  f(i);
+  cout << "\n\n###########################\n\n";
+  aProgression(12, 3);
 
   return 0;
 }
