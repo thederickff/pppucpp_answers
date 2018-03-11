1e0a6a7a7ac920e83087211fc48352e06aa44379
derickfelix <derickfelix4321@gmail.com>
2018-03-11 12:13:03 -0300

    Chapter 09: create drill 9.4.1.cpp

---
+++ Part01/09.Technicalities-Classes-etc/drill/9.4.1.cpp
@@ -0,0 +1,46 @@
+#include "../../std_lib_facilities.h"
+
+struct Date {
+  int y;  // year
+  int m;  // month in year
+  int d;  // day of month
+};
+
+bool is_valid(const Date& date)
+{
+  if (date.d <= 0 || date.d > 31) return false;
+  if (date.m <= 0 || date.m > 12) return false;
+  return true;
+}
+
+void add_day(Date& date)
+{
+  date.d += 1;
+}
+
+ostream& operator<<(ostream& os, const Date& date)
+{
+  return os << '(' << date.y << ',' << date.m << ',' << date.d << ')';
+}
+
+int main()
+{
+  Date today;
+  today.y = 1978;
+  today.m = 6;
+  today.d = 25;
+  Date tomorrow = today;
+  add_day(tomorrow);
+  Date invalid;
+  invalid.y = 2004;
+  invalid.m = 13;
+  invalid.d = -5;
+
+  cout << today << '\n';
+  cout << tomorrow << '\n';
+  cout << invalid << '\n';
+  add_day(invalid);
+  cout << invalid << '\n';
+
+  return 0;
+}
