5c918f6109a516b0d1ac985e1a55ecb477c28e2d
derickfelix <derickfelix4321@gmail.com>
2018-03-13 22:07:13 -0300

    Chapter 09: create ex02.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex02.cpp
@@ -0,0 +1,19 @@
+/*
+  Desing and implement a Name_pairs class holding(name, age) where name is a
+  string and age is a double. Represent that as a vector<string> (called name)
+  and a vector<double> (called age) member. Provide an input operation
+  read_names() that reads a series of names. Provide a read_ages() operation
+  that prompts the user for an age for each name. Provide a print() operation
+  that prints out the (name[i], age[i]) pairs (one per line) in the order
+  determined by the name vector. Provide sort() operation that sorts the name
+  vector in alphabetical order and reorganizes age vector to match. Implement
+  all "operations" as member functions. Test the class (of course: test early
+  and often).
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+  
+  return 0;
+}
