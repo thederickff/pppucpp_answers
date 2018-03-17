1257d98e6b4349d7899a888f7408b341e828b321
derickfelix <derickfelix4321@gmail.com>
2018-03-17 19:40:43 -0300

    Chapter 09: solve ex04.cpp
    Look at the headache-inducing last example of §8.4. Indent it properly and
    explain the meaning of each construct. Note that the example doesn't do
    anything meaningful; it is pure obfuscation.

--- Part01/09.Technicalities-Classes-etc/ex04.cpp
+++ Part01/09.Technicalities-Classes-etc/ex04.cpp
@@ -5,20 +5,36 @@
 */
 #include "../std_lib_facilities.h"
 
-// dangerously ugly code
-struct X {
-void f(int x) {
-struct Y {
-int f() { return 1; } int m; };
-int m;
-m=x; Y m2;
-return f(m2.f()); }
-int m; void g(int m) {
-if (m) f(m+2); else {
-g(m+2); }}
-X() { } void m3() {
-} void main() {
-X a; a.f(2);}
+// Not too dangerously ugly code anymore
+struct X
+{
+  void f(int x)  {
+    struct Y
+    {
+      int f() {
+        return 1;
+      }
+      int m;
+    };
+    int m;
+    m = x;
+    Y m2;
+    return f(m2.f());
+  }
+  int m;
+  void g(int m) {
+    if (m)
+      f(m+2);
+    else {
+      g(m+2);
+    }
+  }
+  X() { }
+  void m3() { }
+  void main() {
+    X a;
+    a.f(2);
+  }
 };
 
 int main()
