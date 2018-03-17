c29b4de56df6f8e7ed7d061f575abf74ba6f20c6
derickfelix <derickfelix4321@gmail.com>
2018-03-17 19:35:10 -0300

    Chapter 09: create ex04.cp

---
+++ Part01/09.Technicalities-Classes-etc/ex04.cpp
@@ -0,0 +1,28 @@
+/*
+  Look at the headache-inducing last example of §8.4. Indent it properly and
+  explain the meaning of each construct. Note that the example doesn't do
+  anything meaningful; it is pure obfuscation.
+*/
+#include "../std_lib_facilities.h"
+
+// dangerously ugly code
+struct X {
+void f(int x) {
+struct Y {
+int f() { return 1; } int m; };
+int m;
+m=x; Y m2;
+return f(m2.f()); }
+int m; void g(int m) {
+if (m) f(m+2); else {
+g(m+2); }}
+X() { } void m3() {
+} void main() {
+X a; a.f(2);}
+};
+
+int main()
+{
+
+  return 0;
+}
