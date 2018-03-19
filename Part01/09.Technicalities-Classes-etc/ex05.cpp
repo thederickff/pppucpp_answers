c0f62279f42200542c3b0282e171200e4e2a84e4
derickfelix <derickfelix4321@gmail.com>
2018-03-19 08:05:44 -0300

    Chapter 09: create ex05.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex05.cpp
@@ -0,0 +1,19 @@
+/*
+  This exercise and the next few require you to design and implement a Book
+  class, such as you can imagine as part of software for a library. Class book
+  should have members for the ISBN, title, author, and copyright date. Also
+  store data on whether or not the is checked out. Create functions for
+  returning those data values. Create functions for checking a book in and out.
+  Do simple validation of data entered into a book; for example, accept ISBNs
+  only of the form n-n-n-x where n is an integer and x is a digit or a letter.
+  Store an ISBN as a string.
+*/
+#include "../std_lib_facilities.h"
+
+
+int main()
+{
+
+
+  return 0;
+}
