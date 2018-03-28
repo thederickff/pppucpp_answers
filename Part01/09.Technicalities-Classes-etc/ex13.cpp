2fdb63055c08ed10c275f17ecd5bfdb00f93ff41
derickfelix <derickfelix4321@gmail.com>
2018-03-27 22:08:54 -0300

    Chapter 09: create ex13.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex13.cpp
@@ -0,0 +1,16 @@
+/*
+  Design and implement a rational number class, Rational. A rational number has
+  two parts: a numerator and a denominator, for example, 5/6 (five-sixths, also
+  known as approximately .83333). Look up the definition if you need to. Provide
+  assignment, addition, subtraction, multiplication, division, and equality
+  operators. Also, provide a conversion to double. Why would people want to use
+  a Rational class?
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
