a7ef8da3c870e41ff825086bf41a9d05e337b619
derickfelix <derickfelix4321@gmail.com>
2018-03-30 14:19:22 -0300

    Chapter 09: solve ex18.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex18.cpp
@@ -0,0 +1,7 @@
+/*
+  Give an example of a calculation where a Rational gives a mathematically
+  better result than double.
+
+  Rational gives a more accurate result than double cuz some floating-point
+  numbers cannot be represented in binary accurately.
+*/
