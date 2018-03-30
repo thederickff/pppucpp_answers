f34b58e334390691be90aa325d2d8eb3452ae94c
derickfelix <derickfelix4321@gmail.com>
2018-03-30 13:41:17 -0300

    Chapter 09: solve ex15.cpp
    Refine the Money class by adding a currency (given as a constructor argument).
    Accept a floating-point initializer as long as it can be exactly represented
    as a long int. Don't accept illegal operations. For example, Money*Money
    doesn't make sense, and USD1.23+DKK5.00 makes sense only if you provide a
    conversion table defining the conversion factor between U.S. dollars (USD) and
    Danish kroner (DKK).

--- Part01/09.Technicalities-Classes-etc/ex15.cpp
+++ Part01/09.Technicalities-Classes-etc/ex15.cpp
@@ -16,6 +16,7 @@ enum class Currency
 string currency_text[] = {
   "AUD", "BRL", "CAD", "DKK", "EUR", "GBP", "USD"
 };
+int size_currency_text = sizeof(currency_text) / sizeof(string);
 
 class Money
 {
@@ -31,13 +32,20 @@ public:
   Money() : m_Currency(Currency::USD), m_Cents(0) { }
   Money(Currency currency, double value);
   long int getCents() const { return m_Cents; }
+  int getCurrencyIndex() const { return int(m_Currency); }
+
 private:
   Currency m_Currency;
   long int m_Cents;
 };
+
 ostream& operator<<(ostream& stream, const Money& money);
 istream& operator>>(istream& stream, Money& money);
+Money operator+(const Money& a, const Money& b);
+Money operator-(const Money& a, const Money& b);
+int get_currency_index(string currency);
 
+// Implementation
 Money::Money(Currency currency, double value) : m_Currency(currency)
 {
   if (value < 0) {
@@ -52,7 +60,7 @@ ostream& operator<<(ostream& stream, const Money& money)
   double value = double(money.getCents()) / 100;
   int dollars = int(value);
 
-  stream << '$' << dollars << '.';
+  stream << currency_text[money.getCurrencyIndex()] << " " << dollars << '.';
   double cents = (value - dollars) * 100;
   stream << (cents < 10 ? "0" : "") << cents;
   return stream;
@@ -60,31 +68,48 @@ ostream& operator<<(ostream& stream, const Money& money)
 
 istream& operator>>(istream& stream, Money& money)
 {
-  /*char ch1;
+  string currency;
   double value;
-  stream >> ch1 >> value;
+  stream >> currency >> value;
   if (!stream) return stream;
-  if (ch1 != '$') {
+
+  if (get_currency_index(currency) == -1) {
     stream.clear(ios_base::failbit);
     return stream;
   }
 
-  money = value;
-  */
+  money = Money {Currency(get_currency_index(currency)), value};
 
   return stream;
+}
+
+int get_currency_index(string currency)
+{
+  for (int i = 0; i < size_currency_text; i++) {
+    if (currency == currency_text[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void print_currencies()
+{
+  cout << "AVAIABLE TYPES: ";
+  for (int i = 0; i < size_currency_text; ++i) {
+    cout << currency_text[i] << " ";
+  }
+  cout << '\n';
 }
 
 int main()
 {
   try {
-    Money food {Currency::USD, 3.11};
-
-    cout << food << '\n';
-    //Money input;
-    //cout << "Input a money: e.g., $4.23\n";
-    //cin >> input;
-    //cout << "Output: " << input << '\n';
+    print_currencies();
+    Money input;
+    cout << "Input a money: e.g., USD 4.23\n";
+    cin >> input;
+    cout << "Output: " << input << '\n';
 
   } catch (Money::InvalidMoney& e) {
     e.what();
