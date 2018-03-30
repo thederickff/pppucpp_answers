6bf881b57246fdede3d61ca7341797533ec563ce
derickfelix <derickfelix4321@gmail.com>
2018-03-30 14:10:28 -0300

    Chapter 09: solve ex17.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex17.cpp
@@ -0,0 +1,8 @@
+/*
+  Give an example of a calculation where a Rational gives a mathematically
+  better result than Money.
+
+  Let's say you want to divide 2 pizzas to, say 14 friends, and other friend
+  come with more 3 pizzas and 8 more friends a rational calculation would give
+  a better result.
+*/
