87dbad285095eaee5026eb36b57fcd6328e57470
derickfelix <derickfelix4321@gmail.com>
2018-03-30 13:58:35 -0300

    Chapter 09: solve ex16.cpp
    Define an input operator (>>) that reads monetary amounts with currency
    denominations, such as USD1.23 and DKK5.00, into a Money variable. Also
    define a corresponding output operator (>>).

---
+++ Part01/09.Technicalities-Classes-etc/ex16.cpp
@@ -0,0 +1,118 @@
+/*
+  Define an input operator (>>) that reads monetary amounts with currency
+  denominations, such as USD1.23 and DKK5.00, into a Money variable. Also
+  define a corresponding output operator (>>).
+
+  Actually it was done on the previous exercise
+*/
+#include "../std_lib_facilities.h"
+
+enum class Currency
+{
+  AUD, BRL, CAD, DKK, EUR, GBP, USD
+};
+
+string currency_text[] = {
+  "AUD", "BRL", "CAD", "DKK", "EUR", "GBP", "USD"
+};
+int size_currency_text = sizeof(currency_text) / sizeof(string);
+
+class Money
+{
+public:
+  class InvalidMoney
+  {
+    public:
+      InvalidMoney(double value) : m_Value(value) { }
+      void what() { cout <<  "Invalid money input for " << m_Value << '\n'; }
+    private:
+      double m_Value;
+  };
+  Money() : m_Currency(Currency::USD), m_Cents(0) { }
+  Money(Currency currency, double value);
+  long int getCents() const { return m_Cents; }
+  int getCurrencyIndex() const { return int(m_Currency); }
+
+private:
+  Currency m_Currency;
+  long int m_Cents;
+};
+
+ostream& operator<<(ostream& stream, const Money& money);
+istream& operator>>(istream& stream, Money& money);
+Money operator+(const Money& a, const Money& b);
+Money operator-(const Money& a, const Money& b);
+int get_currency_index(string currency);
+
+// Implementation
+Money::Money(Currency currency, double value) : m_Currency(currency)
+{
+  if (value < 0) {
+    throw InvalidMoney(value);
+  }
+  double cents = (value - int(value)) * 100;
+  m_Cents = int(cents + 0.5) + (int(value) * 100); //  4/5 rounding rule
+}
+
+ostream& operator<<(ostream& stream, const Money& money)
+{
+  double value = double(money.getCents()) / 100;
+  int dollars = int(value);
+
+  stream << currency_text[money.getCurrencyIndex()] << " " << dollars << '.';
+  double cents = (value - dollars) * 100;
+  stream << (cents < 10 ? "0" : "") << cents;
+  return stream;
+}
+
+istream& operator>>(istream& stream, Money& money)
+{
+  string currency;
+  double value;
+  stream >> currency >> value;
+  if (!stream) return stream;
+
+  if (get_currency_index(currency) == -1) {
+    stream.clear(ios_base::failbit);
+    return stream;
+  }
+
+  money = Money {Currency(get_currency_index(currency)), value};
+
+  return stream;
+}
+
+int get_currency_index(string currency)
+{
+  for (int i = 0; i < size_currency_text; i++) {
+    if (currency == currency_text[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void print_currencies()
+{
+  cout << "AVAIABLE TYPES: ";
+  for (int i = 0; i < size_currency_text; ++i) {
+    cout << currency_text[i] << " ";
+  }
+  cout << '\n';
+}
+
+int main()
+{
+  try {
+    print_currencies();
+    Money input;
+    cout << "Input a money: e.g., USD 4.23\n";
+    cin >> input;
+    cout << "Output: " << input << '\n';
+
+  } catch (Money::InvalidMoney& e) {
+    e.what();
+  }
+
+  return 0;
+}
