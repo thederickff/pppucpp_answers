2d255f4ada684da02f3247a2f6f7a56f31a2047e
derickfelix <derickfelix4321@gmail.com>
2018-03-30 08:35:23 -0300

    Chapter 09: create ex14.cpp

---
+++ Part01/09.Technicalities-Classes-etc/ex14.cpp
@@ -0,0 +1,15 @@
+/*
+  Design and implement a Money class for calculations involving dollars and
+  centsw here arithmetic has to be accurate to the last cent using the 4/5
+  rounding rule (.5 of a cent rounds up; anything less than .5 rounds down).
+  Represent a monetary amount as a number of cents in a long int, but input
+  and output as dollars and cents, e.g., $123.45. Do not worry about amounts
+  that don't fit into a long int.
+*/
+#include "../std_lib_facilitites.h"
+
+int main()
+{
+
+  return 0;
+}
