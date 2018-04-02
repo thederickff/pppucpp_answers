a303e55c4b52d3e2d4b0233809319a3b22191a07
derickfelix <derickfelix4321@gmail.com>
2018-04-02 09:09:32 -0300

    Chapter 10: create drill 02

--- Part02/10.Input-and-Output-Streams/drill/main.cpp
+++ Part02/10.Input-and-Output-Streams/drill/main.cpp
@@ -1,6 +1,9 @@
 /*
-  1. Start a program to work with points, discussed in §10.4. Begin by defining the
-     data type Point that has two coordinate members x and y.
+  1. Start a program to work with points, discussed in §10.4. Begin by defining
+     the data type Point that has two coordinate members x and y.
+  2. Using the code and discussion in §10.4, prompt the user to input seven
+     (x,y) pairs. As the data entered, store it in a vector of Points called
+     original_points to see what it looks like.
 */
 #include "../std_lib_facilities.h"
 
@@ -10,6 +13,27 @@ struct Point {
 
 int main()
 {
+  vector<Point> original_points;
+  cout << "Input seven coordinates in the format (x,y).\n";
+  for (int i = 0; i < 7; ++i) {
+    cout << "#" << (i+1) << ": ";
+    char ch1, ch2, ch3;
+    int x, y;
+    cin >> ch1 >> x >> ch2 >> y >> ch3;
+    if (ch1 != '(' || ch2 != ',' || ch3 != ')') {
+      cin.clear(ios_base::failbit);
+      break;
+    }
+    Point point;
+    point.x = x;
+    point.y = y;
+    original_points.push_back(point);
+  }
+
+  for (const Point& point : original_points) {
+    cout << '(' << point.x << ',' << point.y << ')';
+  }
+  cout << '\n';
 
   return 0;
 }
