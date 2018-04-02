4c717ec54759e4185b437ee043c7df34e0a84915
derickfelix <derickfelix4321@gmail.com>
2018-04-02 08:56:33 -0300

    Chapter 10: create drill 01

---
+++ Part02/10.Input-and-Output-Streams/drill/main.cpp
@@ -0,0 +1,15 @@
+/*
+  1. Start a program to work with points, discussed in §10.4. Begin by defining the
+     data type Point that has two coordinate members x and y.
+*/
+#include "../std_lib_facilities.h"
+
+struct Point {
+  int x, y;
+};
+
+int main()
+{
+
+  return 0;
+}
