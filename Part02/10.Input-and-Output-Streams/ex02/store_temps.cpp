338a19aafb50fa18528225b1a8bb42da225496a5
derickfelix <derickfelix4321@gmail.com>
2018-04-05 22:16:36 -0300

    Chapter 10: create ex02

---
+++ Part02/10.Input-and-Output-Streams/ex02/store_temps.cpp
@@ -0,0 +1,14 @@
+/*
+  Write a program that creates a file of data in the format of the temperature
+  Reading type defined in §10.5. For testing, fill the file with at least 50
+  "temperature readings". Call this program store_temps.cpp and the file it
+  creates raw_temps.txt.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
