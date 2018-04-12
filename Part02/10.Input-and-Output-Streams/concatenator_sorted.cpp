9f487d3c1aa80aa54870336502b99536c8d66bf2
derickfelix <derickfelix4321@gmail.com>
2018-04-11 21:21:55 -0300

    Chapter 10: setup  from exercise 9

---
+++ Part02/10.Input-and-Output-Streams/concatenator_sorted.cpp
@@ -0,0 +1,11 @@
+/*
+  Write a program that takes two files containing sorted whitespace-separated
+  words and merges them, preserving order.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+  return 0;
+}
