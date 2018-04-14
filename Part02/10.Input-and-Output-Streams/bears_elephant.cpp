6f403b0691c7355c1b326e61e8c11b7e764a061f
derickfelix <derickfelix4321@gmail.com>
2018-04-13 23:24:22 -0300

    Chapter 10: setup exercise 11

---
+++ Part02/10.Input-and-Output-Streams/bears_elephant.cpp
@@ -0,0 +1,11 @@
+/*
+  Write a program that produces the sum of all whitespace-separated integers in
+  a text file. For example, bears: 17 elephants 9 end should output 26.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+  return 0;
+}
