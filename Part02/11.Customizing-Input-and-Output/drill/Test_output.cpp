af262fb729e5a0622c23df5c2d1d292dcbf5958a
derickfelix <derickfelix4321@gmail.com>
2018-04-16 20:39:11 -0300

    Chapter 11: drill 04
    Did you line up your output in columns using the tab character? If not,
    do it.

--- Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
+++ Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
@@ -1,8 +1,10 @@
 /*
   1. Start a program called Test_output.cpp. Declare an integer birth_year and
-  assign it the year you were born.
+     assign it the year you were born.
   2. Output your birth_year in decimal, hexadecimal, and octal form.
   3. Label each value with the name of the base used.
+  4. Did you line up your output in columns using the tab character? If not,
+     do it.
 */
 #include "../std_lib_facilities.h"
 
@@ -10,8 +12,8 @@ int main()
 {
   int birth_year = 2001;
 
-  cout << "Decimal: " << birth_year << ' ' <<
-  hex << "Hexadecimal: " << birth_year << ' ' <<
+  cout << "Decimal: " << birth_year << '\t' <<
+  hex << "Hexadecimal: " << birth_year << '\t' <<
   oct << "Octal: " << birth_year << '\n';
 
   return 0;
