467086076e011fe8679c76975f46e44f03931554
derickfelix <derickfelix4321@gmail.com>
2018-04-16 20:37:17 -0300

    Chapter 11: drill 03
    Label each value with the name of the base used.

--- Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
+++ Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
@@ -2,6 +2,7 @@
   1. Start a program called Test_output.cpp. Declare an integer birth_year and
   assign it the year you were born.
   2. Output your birth_year in decimal, hexadecimal, and octal form.
+  3. Label each value with the name of the base used.
 */
 #include "../std_lib_facilities.h"
 
@@ -9,7 +10,9 @@ int main()
 {
   int birth_year = 2001;
 
-  cout << birth_year << ' ' << hex << birth_year << ' ' << oct << birth_year << '\n';
+  cout << "Decimal: " << birth_year << ' ' <<
+  hex << "Hexadecimal: " << birth_year << ' ' <<
+  oct << "Octal: " << birth_year << '\n';
 
   return 0;
 }
