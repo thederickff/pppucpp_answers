9ee56c4a1088983bb1974787594bb1b4cff22663
derickfelix <derickfelix4321@gmail.com>
2018-04-16 20:33:44 -0300

    Chapter 11: drill 01
    Start a program called Test_output.cpp. Declare an integer birth_year and
    assign it the year you were born.

---
+++ Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
@@ -0,0 +1,12 @@
+/*
+  1. Start a program called Test_output.cpp. Declare an integer birth_year and
+  assign it the year you were born.
+*/
+#include "../std_lib_facilities.h"
+
+int main()
+{
+  int birth_year = 2001;
+
+  return 0;
+}
