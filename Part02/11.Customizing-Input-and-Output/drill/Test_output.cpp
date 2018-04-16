019607fda280658296a0cc9464caa69970d3703f
derickfelix <derickfelix4321@gmail.com>
2018-04-16 20:34:45 -0300

    Chapter 11: drill 02
    Output your birth_year int decimal, hexadecimal, and octal form.

--- Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
+++ Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
@@ -1,12 +1,15 @@
 /*
   1. Start a program called Test_output.cpp. Declare an integer birth_year and
   assign it the year you were born.
+  2. Output your birth_year in decimal, hexadecimal, and octal form.
 */
 #include "../std_lib_facilities.h"
 
 int main()
 {
   int birth_year = 2001;
+
+  cout << birth_year << ' ' << hex << birth_year << ' ' << oct << birth_year << '\n';
 
   return 0;
 }
