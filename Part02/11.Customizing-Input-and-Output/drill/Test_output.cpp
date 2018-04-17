44f65a06182084c747f614bf0a731497cc246e39
derickfelix <derickfelix4321@gmail.com>
2018-04-16 21:05:47 -0300

    Chapter 11: drill 10
    Make a simple table including the last name, first name, telephone number, and
    email address for yourself and at least five of your friends. Experiment with
    different field widths until you are sastified that the table is well
    presented.

--- Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
+++ Part02/11.Customizing-Input-and-Output/drill/Test_output.cpp
@@ -16,7 +16,11 @@
      Explain the results.
   9. Write some code to print the number 1234567.89 three times, first using the
      defaultfloat, then fixed, then scientific forms. Which output form presents
-     the user with the most accurate representation? Explain why
+     the user with the most accurate representation? Explain why.
+ 10. Make a simple table including last name, first name, telephone number, and
+     email address for yourself and at least five of your friends. Experiment
+     with different field widths until your are sastified that the table is well
+     presented.
 */
 #include "../std_lib_facilities.h"
 
@@ -44,6 +48,7 @@ int main()
   */
   cout << a << '\t' << b << '\t' << c << '\t' << d << '\n';
 
+  cout << "\n-- Drill 09 --\n";
   float number = 1234567.89;
 
   /*
@@ -53,6 +58,15 @@ int main()
     It happens because the precision was set to 6 digits.
   */
   cout << number << '\t' << fixed << number << '\t' << scientific << number << '\n';
+
+  cout << "\n-- Drill 10 --\n";
+  cout << "Last name\tFirst Name\tTelephone number\tEmail Address\n";
+  cout << "Felix" << setw(10) << "\tDerick" << setw(20) << "\t202-555-0112" << setw(30) << "\temail@example.com\n";
+  cout << "Wayne" << setw(10) << "\tJill" << setw(20) << "\t202-555-0191" << setw(25) << "\texample@mail.com\n";
+  cout << "Brennan" << setw(10) << "\tJanis" << setw(20) << "\t202-555-0199" << setw(20) << "\tumane@zome.com\n";
+  cout << "Alannis" << setw(10) << "\tMerry" << setw(20) << "\t202-555-0140" << setw(30) << "\tleptoac@coldmail.com\n";
+  cout << "Grahame" << setw(10) << "\tHailie" << setw(20) << "\t202-555-0177" << setw(30) << "\tuser28527@bmail.com\n";
+  cout << "Claire" << setw(10) << "\tCole" << setw(20) << "\t202-555-0142" << setw(30) << "\tuser37826@bmail.com\n";
 
   return 0;
 }
