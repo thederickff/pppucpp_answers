7497db0d72a92527f7f62456fdeffae193a5c8ca
derickfelix <derickfelix4321@gmail.com>
2018-04-19 09:08:05 -0300

    Chapter 11: create ex03.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex03.cpp
@@ -0,0 +1,13 @@
+/*
+  Write a program that removes all vowels from a file ("disemvowels"). For example,
+  Once upon a time! becomes nc pn tm!. Surprisingly often, the result is still
+  readable; try it on your friends.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
