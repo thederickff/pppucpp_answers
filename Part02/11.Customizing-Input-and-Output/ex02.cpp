8b27f911848575ffb129d8d714039584e40b2ac7
derickfelix <derickfelix4321@gmail.com>
2018-04-18 22:39:39 -0300

    Chapter 11: solve exercise 02
    Write a program that given a file name and a word outputs each line that
    contains that word together with the line number. Hint: getline().

--- Part02/11.Customizing-Input-and-Output/ex02.cpp
+++ Part02/11.Customizing-Input-and-Output/ex02.cpp
@@ -6,7 +6,32 @@
 
 int main()
 {
-  
+  string filename, word;
+  cout << "To see every line that contains a specific word of a file, type: [filename] [word]\n> ";
+  cin >> filename >> word;
+
+  ifstream ifs {filename};
+  if (!ifs) error("Failed to read filename ", filename);
+
+  int line_num = 1;
+  while (ifs) {
+    string line;
+    getline(ifs, line);
+
+    stringstream ss(line);
+    ostringstream osw;
+    for (char ch; ss >> ch; ) {
+      if (isspace(ch)) {
+        osw.str("");
+      }
+      osw << ch;
+      if (word == osw.str()) {
+        cout << line_num << " " << line << '\n';
+      }
+    }
+    line_num++;
+  }
+
 
   return 0;
 }
