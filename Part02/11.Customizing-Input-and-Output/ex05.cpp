57bff999ea0c4784283e03f325ad8d17e6efa27f
derickfelix <derickfelix4321@gmail.com>
2018-04-19 09:39:25 -0300

    Chapter 11: create ex04.cpp & ex05.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex05.cpp
@@ -0,0 +1,13 @@
+/*
+  Write a program that reads strings  and for each  string outputs the character
+  classification of each character,  as defined by the character  classification
+  functions  presented  in  §11.6.  Note  that  a  character  can  have  several
+  classifications (e.g, x is both a letter and an alphanumeric).
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+  return 0;
+}
