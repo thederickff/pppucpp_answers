61a2714d0dbeb7771062ff7b79a2ae44997ec89a
derickfelix <derickfelix4321@gmail.com>
2018-04-18 22:12:18 -0300

    Chapter 11: create ex02.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex02.cpp
@@ -0,0 +1,12 @@
+/*
+  Write a program that given a file name and a word outputs each line that
+  contains that word together with the line number. Hint: getline().
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
