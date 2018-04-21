bbf96d25651a9e8d6c54d5f8b23d7b3524024a28
derickfelix <derickfelix4321@gmail.com>
2018-04-21 09:38:03 -0300

    Chapter 11: create ex07.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex07.cpp
@@ -0,0 +1,39 @@
+/*
+  Modify the program from the previous exercise so that it replaces don't with
+  do not, can't with cannot, etc.; leaves hyphens within words intact (so that
+  we get "do not use the as-if rule"); and converts all characters to lower
+  case.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  string filename;
+  cout << "Input a filename: ";
+  cin >> filename;
+
+  ifstream ifs {filename};
+  if (!ifs) error("Could not open file for read: ", filename);
+
+  ofstream ofs {string("output_") + filename};
+  if (!ofs) error("Could not write to output file!");
+
+  while (ifs) {
+    string line;
+    getline(ifs, line);
+
+    for (char& ch : line) {
+      if (ch == '\"' || !ispunct(ch)) {
+        ofs << ch;
+      } else {
+        ofs << ' ';
+      }
+    }
+
+    ofs << '\n';
+  }
+
+  cout << "File written successfully\n";
+
+  return 0;
+}
