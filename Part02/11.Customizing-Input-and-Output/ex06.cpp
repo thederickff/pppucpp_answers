334a513b2794bbcd27306080966bd6ba7990f976
derickfelix <derickfelix4321@gmail.com>
2018-04-21 09:20:28 -0300

    Chapter 11: create ex06.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex06.cpp
@@ -0,0 +1,15 @@
+/*
+  Write a program that replaces punctuation with whitespace. Consider . (dot),
+  semicolon (;), , (comma), ? (question mark), - (dash), ' (single quote)
+  punctuation characters. Don't modify characters within a pair of double quotes
+  ("). For example, "- dont't use the as-if rule." becomes " don t use the as if
+  rule".
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
