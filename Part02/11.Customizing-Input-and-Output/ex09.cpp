f011754d4ff24bdc579340804d5bb482f5aac4a2
derickfelix <derickfelix4321@gmail.com>
2018-04-22 10:14:34 -0300

    Chapter 11: create ex09.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex09.cpp
@@ -0,0 +1,14 @@
+/*
+  Split the binary I/O program from §11.3.2 into two; one program that converts
+  an ordinary text file into binary and one program that reads binary and
+  converts it into text. Test these programs by comparing text file with what
+  you get by converting it to binary and back.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
