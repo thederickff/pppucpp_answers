4442edb4891ea6e7d38729d502bef60f1ad237fa
derickfelix <derickfelix4321@gmail.com>
2018-04-22 11:48:05 -0300

    Chapter 11: fix 'i' miss-typing erro

--- Part02/11.Customizing-Input-and-Output/ex11.cpp
+++ Part02/11.Customizing-Input-and-Output/ex11.cpp
@@ -1,4 +1,4 @@
-i/*
+/*
   Write a function vector<string> split(const string& s, const string& w) that
   returns a vector of whitespace-separated substrings from the argument s, where
   whitespace is defined as "ordinary whitespace" plus the characters in w.
