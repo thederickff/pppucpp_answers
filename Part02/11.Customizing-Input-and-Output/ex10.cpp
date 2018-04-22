f00d84387df9827b60ab7ee2e37edac367326ca5
derickfelix <derickfelix4321@gmail.com>
2018-04-22 11:44:47 -0300

    Chapter 11: solve exercise 10 and 11
    10: Write a function vector<string> split(const string& s) that returns a vector
    	of whitespace-separated substrings from the argument s.
    
    11: Write a function vector<string> split(const string& s, const string& w) that
    	returns a vector of whitespace-separated substrings from the argument s, where
    	whitespace is defined as "ordinary whitespace" plus the characters in w.

--- Part02/11.Customizing-Input-and-Output/ex10.cpp
+++ Part02/11.Customizing-Input-and-Output/ex10.cpp
@@ -4,9 +4,24 @@
 */
 #include "std_lib_facilities.h"
 
+vector<string> split(const string& s)
+{
+  vector<string> split;
+  istringstream is {s};
+
+  for (string w; is >> w; )
+    split.push_back(w);
+
+  return split;
+}
+
 int main()
 {
+  vector<string> words = split("if you write anything on your computer, you'll need to get gram**rly");
 
+  for (string& w : words) {
+    cout << w << '\n';
+  }
 
   return 0;
 }
--- Part02/11.Customizing-Input-and-Output/ex11.cpp
+++ Part02/11.Customizing-Input-and-Output/ex11.cpp
@@ -1,13 +1,49 @@
-/*
+i/*
   Write a function vector<string> split(const string& s, const string& w) that
   returns a vector of whitespace-separated substrings from the argument s, where
   whitespace is defined as "ordinary whitespace" plus the characters in w.
 */
-#include "std_lib_facilities.h"
+//#include "std_lib_facilities.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(const string& s, const string& w)
+{
+  vector<string> split;
+  istringstream is {s};
+
+
+  for (string w; is >> w; ) {
+    ostringstream os;
+    for (char& ch : w) {
+      if (ch == ',') {
+        if (os.str() != "") {
+          split.push_back(os.str());
+        }
+        os.str("");
+      } else {
+        os << ch;
+      }
+    }
+    if (os.str() != "") {
+      split.push_back(os.str());
+    }
+  }
+
+  return split;
+}
 
 int main()
 {
+  vector<string> words = split("if you write any,thing on your comp,uter, you'll need to get ,gram,rly,", ",");
 
+  for (string& w : words) {
+    cout << w << '\n';
+  }
 
   return 0;
 }
