c2afd6649a343c03e18ee02345fae0569df9f26d
derickfelix <derickfelix4321@gmail.com>
2018-04-22 11:30:06 -0300

    Chapter 11: create ex10.cpp and ex11.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex10.cpp
@@ -0,0 +1,12 @@
+/*
+  Write a function vector<string> split(const string& s) that returns a vector
+  of whitespace-sepatated substrings from the argument s.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+
+  return 0;
+}
---
+++ Part02/11.Customizing-Input-and-Output/ex11.cpp
@@ -0,0 +1,13 @@
+/*
+  Write a function vector<string> split(const string& s, const string& w) that
+  returns a vector of whitespace-separated substrings from the argument s, where
+  whitespace is defined as "ordinary whitespace" plus the characters in w.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+
+  return 0;
+}
