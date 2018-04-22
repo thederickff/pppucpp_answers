24cfbb34dec790dc02da0a2d77fccc7a8d5fd9dd
derickfelix <derickfelix4321@gmail.com>
2018-04-22 10:26:05 -0300

    Chapter 11: setup ex09

--- Part02/11.Customizing-Input-and-Output/ex09.cpp
+++ Part02/11.Customizing-Input-and-Output/ex09.cpp
@@ -8,7 +8,32 @@
 
 int main()
 {
-  
+  cout << "Please enter input file name\n";
+  string iname;
+  cin >> iname;
+  ifstream ifs {iname, ios_base::binary}; // note: stream mode
+      // binary tells the stream not to try anything clever with the bytes
+  if (!ifs) error("can't open input file name ", iname);
+
+  // open an ostream for binary ouput to a file:
+  cout << "Please enter output file name\n";
+  string oname;
+  cin >> oname;
+  ofstream ofs {oname, ios_base::binary}; // note: stream mode
+      // binary tells the stream not to try anything clever with the bytes
+  if (!ofs) error("can't open output file ", oname);
+
+  vector<int> v;
+
+  // read from binary file:
+  for (int x; ifs.read(as_bytes(x), sizeof(int)); ) // note: reading bytes
+    v.push_back(x);
+
+  // ... do something with v ...
+
+  // write to binary file:
+  for (int x : v)
+      ofs.write(as_bytes(x), sizeof(int));  // note: writing bytes
 
   return 0;
 }
