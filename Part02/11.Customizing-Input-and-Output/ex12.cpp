c7d48fe86d09bb360ebc7f6e5527ef9f4c3f82ee
derickfelix <derickfelix4321@gmail.com>
2018-04-23 22:31:04 -0300

    Chapter 11: create ex12.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex12.cpp
@@ -0,0 +1,13 @@
+/*
+  Reverse the order of characters in a text file. For example, asdfghjkl becomes
+  lkjhgfdsa. Warning: There is no really good, portable, and efficient way of
+  reading a file backward.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
