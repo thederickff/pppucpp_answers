84c22722c90353c79e837c8a0fa94a655c11b501
derickfelix <derickfelix4321@gmail.com>
2018-04-27 20:46:28 -0300

    Chapter 11: create ex15.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex15.cpp
@@ -0,0 +1,13 @@
+/*
+  Write a program that reads a file of whitespace-separated numbers and outputs
+  a file of numbers using scientific format and precision 8 in four fields of 20
+  characters per line.
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
