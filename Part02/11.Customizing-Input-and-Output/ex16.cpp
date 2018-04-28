0df37773e2f59248fe271afced523e2fa51ae555
derickfelix <derickfelix4321@gmail.com>
2018-04-27 21:30:51 -0300

    Chapter 11: create ex16.cpp

---
+++ Part02/11.Customizing-Input-and-Output/ex16.cpp
@@ -0,0 +1,19 @@
+/*
+  Write a program to read a file of whitspace-separated numbers and output them
+  in order (lowest value first), one value per line. Write a value only once,
+  and if it occurs more than once write the count of its occurences on its line.
+  For example, 7 5 5 7 3 117 5 should give
+
+  3
+  5   3
+  7   2
+  117
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+  
+
+  return 0;
+}
