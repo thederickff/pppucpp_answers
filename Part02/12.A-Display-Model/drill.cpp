54d7427e2b18610c3cbbe2b4b1471bdd24003409
derickfelix <derickfelix4321@gmail.com>
2018-04-29 16:59:23 -0300

    Chapter 12: drill #2

--- Part02/12.A-Display-Model/drill.cpp
+++ Part02/12.A-Display-Model/drill.cpp
@@ -5,6 +5,8 @@
      and include Graph.cpp and Window.cpp in your project.
   2. Now add the examples from §12.7 one by one, testing between each added
      subsection example.
+  3. Go through and make one minor change (e.g, in color, in location, or in
+     number of points) to each of the subsection examples.
 */
 #include "Simple_window.h" // get access to our window library
 #include "Graph.h"         // get access to out graphics library facilities
@@ -16,34 +18,34 @@ int main()
   try {
     using namespace Graph_lib;  // our graphics facilities are in Graph_lib
 
-    Point tl {100, 100};        // to become top left corner of window
+    Point tl {200, 100};        // to become top left corner of window
 
     Simple_window win {tl, 600, 400, "My window"}; // make simple window
 
 
     win.wait_for_button();  // display
 
-    Axis xa {Axis::x, Point{20, 300}, 280, 10, "x axis"}; // make an Axis
+    Axis xa {Axis::x, Point{20, 300}, 410, 10, "x axis"}; // make an Axis
                         // an Axis is a kind of Shape
                         // Axis::x means horizontal
                         // starting at (20, 300)
                         // 280 pixels long
                         // 10 "notches"
                         // label the axis "x axis"
-
+    xa.set_color(Color::dark_blue);
     win.attach(xa);       // attach xa to window, win
     win.set_label("Canvas #2"); // relabel the window
     win.wait_for_button();  // display!
 
     Axis ya {Axis::y, Point{20, 300}, 280, 10, "y axis"};
-    ya.set_color(Color::cyan);            // choose color
+    ya.set_color(Color::dark_blue);            // choose color
     ya.label.set_color(Color::dark_red);  // choose color for the text
 
     win.attach(ya);
     win.set_label("Canvas #3");
     win.wait_for_button();      // display!
 
-    Function sine {sin, 0, 100, Point{20, 150}, 1000, 50, 50}; // sine curve
+    Function sine {sin, 0, 40, Point{20, 150}, 1000, 9, 20}; // sine curve
         // plot sin() in the range[0:100] with (0,0) at (20, 150)
         // using 1000 points; scale x values * 50, scale y values * 50
     win.attach(sine);
@@ -53,31 +55,31 @@ int main()
     sine.set_color(Color::blue);  // we changed our mind about sine's color
 
     Polygon poly; // a polygon; a Polygon is a kind of Shape
-    poly.add(Point{300, 200});  // three points make a triangle
+    poly.add(Point{200, 200});  // three points make a triangle
     poly.add(Point{350, 100});
     poly.add(Point{400, 200});
 
-    poly.set_color(Color::red);
-    poly.set_style(Line_style::dash);
+    poly.set_color(Color::green);
+    poly.set_style(Line_style::dot);
     win.attach(poly);
     win.set_label("Canvas #5");
     win.wait_for_button();
 
-    Rectangle r{Point{200, 200}, 100, 50};  // top left corner, width, height
+    Rectangle r{Point{200, 200}, 200, 25};  // top left corner, width, height
     win.attach(r);
     win.set_label("Canvas #6");
     win.wait_for_button();
 
     Closed_polyline poly_rect;
-    poly_rect.add(Point{100, 50});
+    poly_rect.add(Point{110, 50});
     poly_rect.add(Point{200, 50});
-    poly_rect.add(Point{200, 100});
-    poly_rect.add(Point{100, 100});
+    poly_rect.add(Point{200, 120});
+    poly_rect.add(Point{400, 100});
     win.attach(poly_rect);
     win.set_label("Canvas #6.1");
     win.wait_for_button();
 
-    poly_rect.add(Point{50, 75});
+    poly_rect.add(Point{10, 25});
     win.set_label("Canvas #6.2");
     win.wait_for_button();
 
@@ -88,7 +90,7 @@ int main()
     win.set_label("Canvas #7");
     win.wait_for_button();
 
-    Text t {Point{150, 150}, "Hello, Graphical world!"};
+    Text t {Point{150, 150}, "H&ll0, Gr§ph!c§l w0rld!"};
     win.attach(t);
     win.set_label("Canvas #8");
     win.wait_for_button();
