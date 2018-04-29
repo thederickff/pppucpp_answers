6c2a01ea9724c3df31163384f42f1cc8db24db76
derickfelix <derickfelix4321@gmail.com>
2018-04-29 17:18:21 -0300

    Chapter 12: finish dirll

--- Part02/12.A-Display-Model/drill.cpp
+++ Part02/12.A-Display-Model/drill.cpp
@@ -18,7 +18,7 @@ int main()
   try {
     using namespace Graph_lib;  // our graphics facilities are in Graph_lib
 
-    Point tl {200, 100};        // to become top left corner of window
+    Point tl {350, 110};        // to become top left corner of window
 
     Simple_window win {tl, 600, 400, "My window"}; // make simple window
 
@@ -33,6 +33,7 @@ int main()
                         // 10 "notches"
                         // label the axis "x axis"
     xa.set_color(Color::dark_blue);
+    xa.label.set_color(Color::dark_red);
     win.attach(xa);       // attach xa to window, win
     win.set_label("Canvas #2"); // relabel the window
     win.wait_for_button();  // display!
@@ -45,41 +46,43 @@ int main()
     win.set_label("Canvas #3");
     win.wait_for_button();      // display!
 
-    Function sine {sin, 0, 40, Point{20, 150}, 1000, 9, 20}; // sine curve
+    Function sine {sin, 0, 45, Point{20, 150}, 1000, 9, 20}; // sine curve
         // plot sin() in the range[0:100] with (0,0) at (20, 150)
         // using 1000 points; scale x values * 50, scale y values * 50
     win.attach(sine);
     win.set_label("Canvas #4");
     win.wait_for_button();
 
-    sine.set_color(Color::blue);  // we changed our mind about sine's color
+    sine.set_color(Color::dark_green);  // we changed our mind about sine's color
 
     Polygon poly; // a polygon; a Polygon is a kind of Shape
     poly.add(Point{200, 200});  // three points make a triangle
-    poly.add(Point{350, 100});
+    poly.add(Point{350, 120});
     poly.add(Point{400, 200});
 
-    poly.set_color(Color::green);
+    poly.set_color(Color::yellow);
     poly.set_style(Line_style::dot);
     win.attach(poly);
     win.set_label("Canvas #5");
     win.wait_for_button();
 
     Rectangle r{Point{200, 200}, 200, 25};  // top left corner, width, height
+    r.set_color(Color::yellow);
     win.attach(r);
     win.set_label("Canvas #6");
     win.wait_for_button();
 
     Closed_polyline poly_rect;
-    poly_rect.add(Point{110, 50});
+    poly_rect.add(Point{110, 85});
     poly_rect.add(Point{200, 50});
     poly_rect.add(Point{200, 120});
-    poly_rect.add(Point{400, 100});
+    poly_rect.add(Point{400, 85});
+    poly_rect.set_color(Color::dark_green);
     win.attach(poly_rect);
     win.set_label("Canvas #6.1");
     win.wait_for_button();
 
-    poly_rect.add(Point{10, 25});
+    poly_rect.add(Point{300, 50});
     win.set_label("Canvas #6.2");
     win.wait_for_button();
 
@@ -90,7 +93,8 @@ int main()
     win.set_label("Canvas #7");
     win.wait_for_button();
 
-    Text t {Point{150, 150}, "H&ll0, Gr§ph!c§l w0rld!"};
+    Text t {Point{100, 350}, "H&ll0, Gr§ph!c§l w0rld!"};
+    t.set_color(Color::dark_yellow);
     win.attach(t);
     win.set_label("Canvas #8");
     win.wait_for_button();
@@ -100,16 +104,17 @@ int main()
     win.set_label("Canvas #9");
     win.wait_for_button();
 
-    Image ii {Point{100, 50}, "image.jpg"}; // 400*212-pixel jpg
+    Image ii {Point{500, 50}, "image.jpg"}; // 400*212-pixel jpg
     win.attach(ii);
     win.set_label("Canvas #10");
     win.wait_for_button();
 
-    ii.move(100, 200);
+    ii.move(50, 150);
     win.set_label("Canvas #11");
     win.wait_for_button();
 
     Circle c {Point{100, 200}, 50};
+    c.set_color(Color::green);
     Ellipse e {Point{100, 200}, 75, 25};
     e.set_color(Color::red);
     Mark m {Point{100, 200}, 'x'};
@@ -120,7 +125,7 @@ int main()
         << "; Window size: " << win.x_max() << "*" << win.y_max();
 
     Text sizes {Point{100, 20}, oss.str()};
-
+    sizes.set_color(Color::cyan);
     Image cal {Point{225, 225}, "snow_cpp.jpg"};  // 320*240-pixel gif
     cal.set_mask(Point{40, 40}, 200, 150);        // display center part of image
     win.attach(c);
