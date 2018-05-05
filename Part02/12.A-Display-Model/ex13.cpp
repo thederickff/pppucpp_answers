2346ae93e6b3a1d4c71704ff492199d10ced497e
derickfelix <derickfelix4321@gmail.com>
2018-05-05 19:17:19 -0300

    Chapter 12: create ex13.cpp

---
+++ Part02/12.A-Display-Model/ex13.cpp
@@ -0,0 +1,5 @@
+/*
+  Find a way to add color to the lines from the previous exercise. Make some
+  lines one color and other lines another color or other colors.
+*/
+// Paste code from the previous exercise ...
