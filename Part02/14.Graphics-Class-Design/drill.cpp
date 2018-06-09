4aa53b8e90cf795bdd4734f109e950b425efd883
derickfelix <derickfelix4321@gmail.com>
2018-06-09 09:55:52 -0300

    Chapter 14: create drill 02
    Derive a class D1 from B1 and override vf(). Make a D1 object and call vf()
    and f() for it.

--- Part02/14.Graphics-Class-Design/drill.cpp
+++ Part02/14.Graphics-Class-Design/drill.cpp
@@ -3,6 +3,9 @@
      Define both of these functions within class B1. Implement each function to
      output its name (e.g, B1::vf()). Make the functions public. Make a B1 object
      and call each function.
+
+  2. Derive a class D1 from B1 and override vf(). Make a D1 object and call vf()
+     and f() for it.
 */
 #include <iostream>
 
@@ -11,6 +14,12 @@ class B1
 public:
   virtual void vf();
   void f();
+};
+
+class D1 : public B1
+{
+public:
+  void vf() override;
 };
 
 void B1::vf()
@@ -23,12 +32,22 @@ void B1::f()
   std::cout << "B1::f()" << std::endl;
 }
 
+void D1::vf()
+{
+  std::cout << "D1::vf()" << std::endl;
+}
+
 int main()
 {
   B1 b1;
 
   b1.vf();
   b1.f();
+
+  D1 d1;
+
+  d1.vf();
+  d1.f();
 
   return 0;
 }
