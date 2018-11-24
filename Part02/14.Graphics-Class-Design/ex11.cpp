3cdc28ef93a68c38b92f08d4781281505c6e9c73
derickfelix <derickfelix4321@gmail.com>
2018-11-24 12:18:57 -0500

    create ex11.cpp

---
+++ Part02/14.Graphics-Class-Design/ex11.cpp
@@ -0,0 +1,36 @@
+/*
+    Define a Binary_tree class derived from Shape. Give the number of level
+    as a parameter (levels==0 means no nodes, levels==1 means one node,
+    levels==2 means one top node with two sub-nodes, levels==3 means one
+    top node with two sub-nodes each with two sub-nodes, etc.). Let a node
+    be represented by a small circle. Connect the nodes by line (as is 
+    conventional). P.S. In computer science, trees grow downward from a top
+    node (amusingly, but logically, often called the root).
+*/
+#include "Custom_objects.h"
+
+int main()
+{
+    Simple_window win(Point{250, 60}, win_width, win_height, "Exercise 10 - Chapter 14: Graphics Class Design");
+
+    try
+    {
+        win.wait_for_button();
+    }
+    catch (const exception &e)
+    {
+        Text em(Point{150, 200}, "Exception caught: ");
+        Text emd(Point{280, 200}, e.what());
+        win.attach(em);
+        win.attach(emd);
+        win.wait_for_button();
+    }
+    catch (...)
+    {
+        Text em(Point{150, 200}, "Unknown exception caught!");
+        win.attach(em);
+        win.wait_for_button();
+    }
+
+    return 0;
+}
