5e33e2c8d42bfe5fc04f6e29f79b790281e25957
derickfelix <derickfelix4321@gmail.com>
2018-12-11 20:58:39 -0500

    create ex15.cpp

---
+++ Part02/14.Graphics-Class-Design/ex15.cpp
@@ -0,0 +1,18 @@
+/*
+    Most class hierarchies have nothing to do with graphics. Define a class 
+    Iterator with a pure virtual function next() that returns a double* (see
+    Chapter 17). Now derive Vector_iterator and List_iterator from Iterator
+    so that next() for Vector_iterator yields a pointer to the next element
+    of a vector<double> and List_iterator does the same for a list<double>.
+    You initialize a Vector_iterator with a vector<double> and the first call
+    of next() yields a pointer to its first element, if any. If there is no
+    next element, return 0. Test this by using a function void print(Iterator&)
+    to print the elements of a vector<double> and list<double>.
+*/
+
+int main()
+{
+    
+
+    return 0;
+}
